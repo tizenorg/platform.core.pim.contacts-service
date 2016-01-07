0279981cea4ce9670a21e1a4d2efeb189f106ab3
Jeesun Kim <iamjs.kim@samsung.com>
2016-01-08 08:21:36 +0900

    add test
    Change-Id: Ifc3c3ebd15bf64664655964fcb55679bad6117c1

---
+++ test/test_query.h
@@ -0,0 +1,8 @@
+
+
+int test_query_person_contact(char **argv);
+int test_query_person_number(char **argv);
+int test_query_person_email(char **argv);
+int test_query_with_query_person_contact(char **argv);
+int test_query_with_query_person_number(char **argv);
+int test_query_with_query_person_email(char **argv);
