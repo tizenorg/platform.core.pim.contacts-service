7a10d1691d694b9e6a687dd732cbe07767e006ed
Jongkyu Koo <jk.koo@samsung.com>
2016-01-07 15:05:56 +0900

    Add aggregation suggestion
    Change-Id: I14d6d557bfcd80ba1e94d6bb766fa38b19eaf39c
    Signed-off-by: Jongkyu Koo <jk.koo@samsung.com>

--- test/test_query.h
+++
@@ -1,8 +0,0 @@
-
-
-int test_query_person_contact(char **argv);
-int test_query_person_number(char **argv);
-int test_query_person_email(char **argv);
-int test_query_with_query_person_contact(char **argv);
-int test_query_with_query_person_number(char **argv);
-int test_query_with_query_person_email(char **argv);
