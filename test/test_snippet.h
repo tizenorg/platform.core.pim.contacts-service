a912ce051c6384d3fa4679a7df33ec1a45deef23
Jeesun Kim <iamjs.kim@samsung.com>
2016-01-08 09:59:21 +0900

    add snippet when search with query
    Change-Id: I1708e081d1460a97d2d1ea282d17ebf6c0e9af1a

---
+++ test/test_snippet.h
@@ -0,0 +1,5 @@
+
+int test_snippet_with_query_person_contact(int argc, char **argv);
+int test_snippet_with_query_person(int argc, char **argv);
+int test_snippet_with_query_person_number(char **argv);
+int test_snippet_with_query_person_email(char **argv);
